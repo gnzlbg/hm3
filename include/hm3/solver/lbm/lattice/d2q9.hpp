c3da6acdf8b56e4f026be770037874ce69056151
gnzlbg <gonzalobg88@gmail.com>
2015-11-29 20:04:48 +0100

    typo

--- include/hm3/solver/lbm/lattice/d2q9.hpp
+++ include/hm3/solver/lbm/lattice/d2q9.hpp
@@ -8,137 +8,84 @@ namespace lattice {
 
 /// D2Q9 Lattice:
 ///
-///  6   2   5
+///  7   3   4     NW  N  NE
 ///
-///  3   0   1
+///  0   8   1      W  C  E
 ///
-///  7   4   8
+///  6   2   5     SW  S  SE
 ///
 struct d2q9 : dimensional<2> {
-  static constexpr suint_t no_distributions() noexcept { return 9; }
-  static auto distributions() noexcept {
-    return view::iota(0, no_distributions());
-  }
-  static auto center_node_id() noexcept { return 0; }
-  static auto node_ids() noexcept { return view::iota(0, 9); }
-  static auto node_ids_without_center() noexcept { return view::iota(1, 9); }
-  static auto direct_node_ids() noexcept { return view::iota(1, 5); }
-  static auto diagonal_node_ids() noexcept { return view::iota(5, 9); }
+  static constexpr suint_t size() noexcept { return 9; }
+  static auto all() noexcept { return view::iota(0, size()); }
 
-  /// go from lattice neighbors to tree neighbors and use that to compute the
-  /// offset
-  ///
-  /// TODO: clean this up, maybe reorder the lattice to match the tree
-  /// neighbors?
-  static auto neighbor_offset(suint_t d) noexcept {
-    HM3_ASSERT(d > 0, "distribution 0 has no neighbor offset");
-    d -= 1;
-    suint_t d_ts[] = {1, 3, 0, 2, 3, 2, 0, 1};
-    suint_t d_t    = d_ts[d];
-    using namespace tree;
-    return (d < 4) ? face_neighbors<2>()[d_t] : edge_neighbors<2>()[d_t];
-  }
-  static suint_t opposite_dist(suint_t d) noexcept {
-    suint_t od[9] = {0, 3, 4, 1, 2, 7, 8, 5, 6};
-    return od[d];
-  }
+  // the members of the enum are exported into scope
+  enum indices {
+    W  = 0,
+    E  = 1,
+    S  = 2,
+    N  = 3,
+    NE = 4,
+    SE = 5,
+    SW = 6,
+    NW = 7,
+    C  = 8
+  };
+  static auto without_center() noexcept { return view::iota(0, 8); }
+  static auto direct() noexcept { return view::iota(0, 4); }
+  static auto diagonal() noexcept { return view::iota(4, 8); }
 
-  template <typename Distributions>
-  static constexpr num_t rho(Distributions&& d) noexcept {
-    num_t tmp = 0;
-    RANGES_FOR (auto&& d_i, distributions()) { tmp += d[d_i]; }
-    return tmp;
-  }
+  static constexpr suint_t opposite(suint_t d) noexcept {
+    suint_t stencil[size()] = {0};
 
-  static constexpr num_t p(num_t density, num_t c_squ) noexcept {
-    return density * c_squ;
+    stencil[W]  = E;
+    stencil[E]  = W;
+    stencil[S]  = N;
+    stencil[N]  = S;
+    stencil[NE] = SW;
+    stencil[SE] = NW;
+    stencil[SW] = NE;
+    stencil[NW] = SE;
+    stencil[C]  = C;
+    return stencil[d];
   }
 
-  template <typename Distributions>
-  static constexpr num_t u(Distributions&& d, num_t density) noexcept {
-    return (d[1] + d[5] + d[8] - (d[3] + d[6] + d[7])) / density;
+  static constexpr auto dir(suint_t d) noexcept {
+    using dir_t   = std::array<sint_t, 2>;
+    dir_t dirs[9] = {{{0, 0}}};
+
+    dirs[W] = dir_t{{-1, 0}};
+    dirs[E] = dir_t{{1, 0}};
+    dirs[S] = dir_t{{0, -1}};
+    dirs[N] = dir_t{{0, 1}};
+    dirs[NE] = dir_t{{1, 1}};
+    dirs[SE] = dir_t{{1, -1}};
+    dirs[SW] = dir_t{{-1, -1}};
+    dirs[NW] = dir_t{{-1, 1}};
+    dirs[C] = dir_t{{0, 0}};
+    return dirs[d];
   }
 
-  template <typename Distributions>
-  static constexpr num_t v(Distributions&& d, num_t density) noexcept {
-    return (d[2] + d[5] + d[6] - (d[4] + d[7] + d[8])) / density;
-  }
+  static num_t cs() noexcept { return 1. / std::sqrt(3.); }
+  static num_t cs_2() noexcept { return cs() * cs(); }
 
-  template <typename Distributions>
-  static num_a<2> velocities(Distributions&& d, num_t density) noexcept {
-    num_a<2> tmp;
-    tmp(0) = u(d, density);
-    tmp(1) = v(d, density);
-    return tmp;
-  }
+  static constexpr num_t constants(suint_t d) noexcept {
+    HM3_ASSERT(d >= 0 and d < size(), "distribution is out of bounds");
+    const num_t t_center   = 4. / 9.;
+    const num_t t_direct   = 1. / 9.;
+    const num_t t_diagonal = 1. / 36.;
 
-  template <typename Distributions>
-  static num_a<2> velocities(Distributions&& d) noexcept {
-    return velocities(d, density(d));
-  }
+    num_t t[9] = {0};
 
-  template <typename State, typename CellData>
-  static void load(State&& s, CellData&& cd) noexcept {
-    cd.load("rho", [&](auto c, auto&&) { return rho(&(s.nodes0(c, 0))); });
-    cd.load("u", [&](auto c, auto&&) {
-      return u(&(s.nodes0(c, 0)), rho(&(s.nodes0(c, 0))));
-    });
-    cd.load("v", [&](auto c, auto&&) {
-      return v(&(s.nodes0(c, 0)), rho(&(s.nodes0(c, 0))));
-    });
-  }
-
-  // One-step collision
-  template <typename Distributions>
-  static constexpr void collide(Distributions&& from, Distributions&& to,
-                                num_t c_squ, num_t omega) {
-    // weighting factors (depending on lattice geometry)
-    constexpr num_t t_0 = 4. / 9.;
-    constexpr num_t t_1 = 1. / 9.;
-    constexpr num_t t_2 = 1. / 36.;
-
-    // integral local density:
-    const num_t dloc = rho(from);
-
-    // x-, and y- velocity components:
-    const num_t u_x = u(from, dloc);
-    const num_t u_y = v(from, dloc);
-
-    // n- velocity components (n = lattice node connection vectors)
-    // this is only necessary for clearence, and only 3 speeds would
-    // be necessary
-    const num_t sx[9] = {0., 1., 0., -1., 0., 1., -1., -1., 1.};
-    const num_t sy[9] = {0., 0., 1., 0., -1., 1., 1., -1., -1.};
-    num_t u_n[9]      = {0};
-    RANGES_FOR (auto i, distributions()) { u_n[i] = sx[i] * u_x + sy[i] * u_y; }
-
-    // equilibrium densities:
-    // (this can be rewritten to improve computational performance
-    // considerabely !)
-    const num_t f1    = (2. * c_squ);
-    const num_t f0    = f1 * c_squ;
-    const num_t u_squ = u_x * u_x + u_y * u_y;  // square velocity
-    const num_t f2    = u_squ / f1;
-    const num_t f3    = t_1 * dloc;
-    const num_t f4    = t_2 * dloc;
-
-    num_t n_equ[9] = {0};
-    n_equ[0]       = t_0 * dloc * (1. - f2);  // zero velocity density
-
-    // axis speeds (factor: t_1)
-    RANGES_FOR (auto i, direct_node_ids()) {
-      n_equ[i] = f3 * (1. + u_n[i] / c_squ + std::pow(u_n[i], 2.) / f0 - f2);
-    }
-
-    // diagonal speeds (factor: t_2)
-    RANGES_FOR (auto i, diagonal_node_ids()) {
-      n_equ[i] = f4 * (1. + u_n[i] / c_squ + std::pow(u_n[i], 2.) / f0 - f2);
-    }
-
-    // relaxation step
-    RANGES_FOR (auto n, node_ids()) {
-      to[n] = from[n] + omega * (n_equ[n] - from[n]);
-    }
+    t[W]  = t_direct;
+    t[E]  = t_direct;
+    t[S]  = t_direct;
+    t[N]  = t_direct;
+    t[NE] = t_diagonal;
+    t[SE] = t_diagonal;
+    t[SW] = t_diagonal;
+    t[NW] = t_diagonal;
+    t[C]  = t_center;
+    return t[d];
   }
 };
 
