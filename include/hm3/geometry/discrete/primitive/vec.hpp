68ae063771c3d9da1c25d0c6322bd5224097eafb
gnzlbg <gonzalobg88@gmail.com>
2016-07-26 18:21:41 +0200

    rename geometry::discrete vector to vec

--- include/hm3/geometry/discrete/primitive/vec.hpp
+++ include/hm3/geometry/discrete/primitive/vec.hpp
@@ -1,7 +1,7 @@
 #pragma once
 /// \file
 ///
-/// Vector.
+/// Vector primitive
 #include <hm3/geometry/discrete/concepts.hpp>
 #include <hm3/geometry/discrete/primitive/point/point.hpp>
 
@@ -9,20 +9,20 @@ namespace hm3 {
 namespace geometry {
 namespace discrete {
 
-/// Vector primitive.
-namespace vector_primitive {
+/// Vec primitive.
+namespace vec_primitive {
 
-/// Nd-dimensional Vector.
+/// Nd-dimensional Vec.
 ///
 /// \tparam Nd Number of spatial dimensions.
 template <dim_t Nd>  //
-struct vector
- : point_primitive::point<Nd> {  // TODO: the rank of a vector is not zero?
+struct vec
+ : point_primitive::point<Nd> {  // TODO: the rank of a vec is not zero?
   using point_t = point_primitive::point<Nd>;
   using point_t::point_t;
   using point_t::operator=;
 
-  explicit vector(point_t p) noexcept : point_t(p) {}
+  explicit vec(point_t p) noexcept : point_t(p) {}
   explicit operator point_t() const noexcept {
     return point_t{static_cast<point_t const&>(*this)};
   }
@@ -31,41 +31,41 @@ struct vector
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 
-/// Length of the vector.
+/// Length of the vec.
 template <typename V, typename UV = uncvref_t<V>, dim_t Nd = UV::dimension(),
-          CONCEPT_REQUIRES_(Same<UV, vector<Nd>>{})>
+          CONCEPT_REQUIRES_(Same<UV, vec<Nd>>{})>
 constexpr num_t length(V&& v) noexcept {
   return v().norm();
 }
 
-/// Direction of the vector (the vector itself).
+/// Direction of the vec (the vec itself).
 template <typename V, typename UV = uncvref_t<V>, dim_t Nd = UV::dimension(),
-          CONCEPT_REQUIRES_(Same<UV, vector<Nd>>{})>
+          CONCEPT_REQUIRES_(Same<UV, vec<Nd>>{})>
 constexpr decltype(auto) direction(V&& v) noexcept {
   return v;
 }
 
-/// Normal to the vector (2D only).
+/// Normal to the vec (2D only).
 template <typename V, typename UV = uncvref_t<V>, dim_t Nd = UV::dimension(),
-          CONCEPT_REQUIRES_(Nd == 2 and Same<UV, vector<Nd>>{})>
+          CONCEPT_REQUIRES_(Nd == 2 and Same<UV, vec<Nd>>{})>
 constexpr UV normal(V&& v) noexcept {
   return UV{-v(1), v(0)};
 }
 
-/// Vector cross-product (3D only).
+/// Vec cross-product (3D only).
 template <typename V, typename UV = uncvref_t<V>, dim_t Nd = UV::dimension(),
-          CONCEPT_REQUIRES_(Nd == 3 and Same<UV, vector<Nd>>{})>
+          CONCEPT_REQUIRES_(Nd == 3 and Same<UV, vec<Nd>>{})>
 constexpr UV cross(V&& v0, V&& v1) noexcept {
   return UV{v0().cross(v1())};
 }
 
-}  // namespace vector_primitive
+}  // namespace vec_primitive
 
-using vector_primitive::vector;
+using vec_primitive::vec;
 
 }  // namespace discrete
 
-using discrete::vector;
+using discrete::vec;
 
 }  // namespace geometry
 }  // namespace hm3
